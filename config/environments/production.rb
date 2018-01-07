221f326ec5ecdd51c1eec5b206fc8a4b276ce150
Brian Ruff <thelonewolf198927@gmail.com>
2018-01-06 23:56:43 -0500

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,6 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -90,4 +89,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.assets.compile = true
+  config.serve_static_assets = true
 end
