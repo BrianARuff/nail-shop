d2114915b4937f1b33cfa6039ceff1512e29fa79
Brian Ruff <thelonewolf198927@gmail.com>
2018-01-08 22:01:47 -0500

    test

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,6 @@
 class ImagesController < ApplicationController
 
-  before_action :set_image, only: %i[show edit update destroy]
+  before_action :set_image, only: %i[show ediupdate destroy]
 
   before_action :authenticate_user!
 
